266c4bdf50f9ae02f8658a9cd98dba10ffa228a1
Aliaksei Vidaseu <alexaat@yahoo.co.uk>
2025-11-28 22:07:45 +0000

    update

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
+import init, {generate_pixel} from "./pkg/ray_tracer.js";
+
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
+
+async function run(){
+
+    await init();
+
+    function get_px(x,y){
+        let point = generate_pixel(x,y);
+        const r = point.color.r.toString(16);
+        const g = point.color.g.toString(16);
+        const b = point.color.b.toString(16);
+        
+        const color = `#${r}0${g}0${b}`;
+
+        console.log(color);
+        ctx.fillStyle = color;
+
+        ctx.fillRect(y, x, 50, 50)
+
+
+    }
+
+    get_px(0,0);
+    get_px(0,50);
+    get_px(0,100);
+
+
+    // Convert canvas to data URL
+    const imageData = canvas.toDataURL('image/png');
+    // Link to download image
+    const downloadLink = document.getElementById('downloadLink');
+    downloadLink.href = imageData;
+
+}
+
+run();
+
+
+/*    
+
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+
 ctx.fillStyle = '#FF0000'; // Set color to red
-ctx.fillRect(0, 0, 1, 1); // Draw rectangle
+ctx.fillRect(0, 0, 50, 50); // Draw rectangle
 ctx.fillStyle = '#00FF00'; // Set color to red
-ctx.fillRect(1, 0, 1, 1); // Draw rectangle
+ctx.fillRect(0, 50, 50, 50); // Draw rectangle
 ctx.fillStyle = '#0000FF'; // Set color to red
-ctx.fillRect(2, 0, 1, 1); // Draw rectangle
+ctx.fillRect(0, 100, 50, 50); // Draw rectangle
 
 
 // Convert canvas to data URL
 const imageData = canvas.toDataURL('image/png');
-
 // Link to download image
 const downloadLink = document.getElementById('downloadLink');
 downloadLink.href = imageData;
+*/
+
+
+
+
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,35 @@
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+pub fn generate_pixel(x: u32, y: u32) -> Point {
+    // let r = rand::thread_rng().gen_range(0, 255) as u8;
+    // let g = rand::thread_rng().gen_range(0, 255) as u8;
+    // let b = rand::thread_rng().gen_range(0, 255) as u8;
 
-pub fn generate_pixel(x: u32, y: u32) -> Point{
-    let r = rand::random_range(0..255);
-    let g = rand::random_range(0..255);
-    let b = rand::random_range(0..255);
-    Point { x, y, color: Vector3 { x: r, y: g, z: b } }
+    let r = 255;
+    let g = 0;
+    let b = 0;
+
+    Point {
+        x,
+        y,
+        color: Color { r, g, b },
+    }
 }
 
 #[derive(Debug)]
-struct Point {
-    x: u32,
-    y: u32,
-    color: Color
+#[wasm_bindgen]
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
+    pub color: Color,
 }
 
-#[derive(Debug)]
-struct Vector3 {
-    x: i32, 
-    y: i32,
-    z: i32,
+#[derive(Debug, Clone, Copy)]
+#[wasm_bindgen]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
 }
-
-type Color = Vector3;
