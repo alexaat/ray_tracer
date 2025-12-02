8fc173f871e1348ebcc2a3cbdb29eb797d9111d2
aliakseividaseu <alexaat@yahoo.co.uk>
2025-12-02 18:54:15 +0000

    preview streatch fix

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@ use crate::ColorRGB;
 
 pub const SHAPES: [&str; 4] = ["plane", "block", "sphere", "sylinder"];
 
-pub const IMAGE_WIDTH: u32 = 350;
+//pub const IMAGE_WIDTH: u32 = 350;
 pub const IMAGE_HEIGHT: u32 = 250;
 
 pub const PREVIEW_IMAGE_WIDTH: usize = 150;
